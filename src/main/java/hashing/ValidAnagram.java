130fd3ac8de0047ac38a1242145da2a45b863564
Fernando Cesar <cmnando.silva@gmail.com>
2024-04-03 20:59:45 -0300

    ValidAnagram: improving logic

--- src/main/java/hashing/ValidAnagram.java
+++ src/main/java/hashing/ValidAnagram.java
@@ -16,6 +16,8 @@ https://leetcode.com/problems/valid-anagram/description/
 
 */
 
+import java.util.Arrays;
+
 public class ValidAnagram {
     public static void main(String[] args) {
         boolean answer = isAnagram("rat", "car");
@@ -24,29 +26,38 @@ public class ValidAnagram {
         );
     }
 
-    public static int TOTAL_LETTERS = 26;
-
     public static boolean isAnagram(String s, String t) {
-        int[] count = new int[TOTAL_LETTERS];
+        char[] charsS = s.toCharArray();
+        char[] charsT = t.toCharArray();
+        Arrays.sort(charsS);
+        Arrays.sort(charsT);
 
-        for (char caracter : s.toCharArray()) {
-            count[getCaracterIndex(caracter)]++;
-        }
-
-        for (char caracter : t.toCharArray()) {
-            count[getCaracterIndex(caracter)]--;
-        }
-
-        for(int value : count) {
-            if (value != 0) {
-                return false;
-            }
-        }
-
-        return true;
+        return Arrays.equals(charsS, charsT);
     }
 
-    private static int getCaracterIndex(char caracter) {
-        return caracter - 'a';
-    }
+//    public static int TOTAL_LETTERS = 26;
+//
+//    public static boolean isAnagram(String s, String t) {
+//        int[] count = new int[TOTAL_LETTERS];
+//
+//        for (char caracter : s.toCharArray()) {
+//            count[getCaracterIndex(caracter)]++;
+//        }
+//
+//        for (char caracter : t.toCharArray()) {
+//            count[getCaracterIndex(caracter)]--;
+//        }
+//
+//        for(int value : count) {
+//            if (value != 0) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
+//
+//    private static int getCaracterIndex(char caracter) {
+//        return caracter - 'a';
+//    }
 }
